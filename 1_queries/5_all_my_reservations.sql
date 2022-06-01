e77933852e27b7cb59907bf34bd053dda773bbfa
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-31 23:23:19 -0400

    LighthouseBnB SELECT

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,7 @@
+SELECT reservations.id, title, start_date, cost_per_night, rating) as average_rating
+FROM reservations JOIN properties on reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY reservations.id, properties.id
+ORDER BY start_date
+LIMIT 10;
