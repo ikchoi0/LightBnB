343e4f4610d14fc539a6b2bac7f196688c7ea76c
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-31 23:58:03 -0400

    LighthouseBnB SELECT

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,4 +1,4 @@
-SELECT reservations.id, title, start_date, cost_per_night, rating) as average_rating
+SELECT reservations.id, title, start_date, cost_per_night, AVG(rating) as average_rating
 FROM reservations JOIN properties on reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 1
